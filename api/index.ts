b448cd3f7ce59d00899f2d05fb074d2033e9f015
orlovvvvv <info.kamilorlowski@icloud.com>
2023-05-13 12:52:34 +0200

    Poprawka pod deploya

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { userRouter } from "./user/user.router";
 import { appointmentRouter } from "./apointments/appointment.router";
@@ -18,6 +18,9 @@ const app = express();
 
 app.use(cors())
 app.use(express.urlencoded())
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
 app.use("/api/users", userRouter)
 app.use("/api/appointments", appointmentRouter)
 app.use("/api/forms", formRouter)
